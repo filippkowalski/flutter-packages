7839f65c5f671765206bfd8165bd9acbbbf7095c
Filip <filippkowalski@users.noreply.github.com>
2021-08-11 16:20:48 +0200

    Small improvements

--- packages/fk_onboarding/lib/fk_onboarding.dart
+++
@@ -1,9 +0,0 @@
-library fk_onboarding;
-
-import 'package:fk_common_ui/fk_common_ui.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:scrolling_page_indicator/scrolling_page_indicator.dart';
-
-part 'onboarding_page.dart';
-part 'onboarding_view_page.dart';
